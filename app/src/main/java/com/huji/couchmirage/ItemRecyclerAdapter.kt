cf1b7fa8d1ef1bae9a81ab41cc3591db3faecb5f
brahanu <wassanbrahan@gmail.com>
2020-09-22 13:47:53 +0300

    crash onClickDepartment

--- app/src/main/java/com/huji/couchmirage/ItemRecyclerAdapter.kt
+++ app/src/main/java/com/huji/couchmirage/ItemRecyclerAdapter.kt
@@ -9,6 +9,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import kotlinx.android.synthetic.main.layout_department_single_list_item.view.*
+import kotlinx.android.synthetic.main.layout_department_single_list_item.view.department_name
+import kotlinx.android.synthetic.main.layout_single_list_item.view.*
 
 class ItemRecyclerAdapter (private var listener: OnItemClickListen
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -16,7 +18,7 @@ class ItemRecyclerAdapter (private var listener: OnItemClickListen
     private var items: List<SingleItem> = ArrayList()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val textView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.departement_items_gallery_layout, parent, false)
+            .inflate(R.layout.layout_single_list_item, parent, false)
 
         val viewHol = ItemViewHolder(textView)
         textView.setOnClickListener { v ->
@@ -48,18 +50,18 @@ class ItemRecyclerAdapter (private var listener: OnItemClickListen
     class ItemViewHolder
     constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        private val departmentImg: ImageView = itemView.department_img
-        private val departmentName: TextView = itemView.department_name
+        private val itemImg: ImageView = itemView.item_img
+        private val itemName: TextView = itemView.item_name
 
         fun bind(item: SingleItem) {
-            departmentName.text = item.itemName
+            itemName.text = item.itemName
             val requestOptions = RequestOptions()
                 .placeholder(R.drawable.ic_launcher_background)
                 .error(R.drawable.ic_launcher_background)
 
             Glide.with(itemView.context).applyDefaultRequestOptions(requestOptions)
                 .load(item.itemImg)
-                .into(departmentImg)
+                .into(itemImg)
 
         }
 
@@ -72,7 +74,7 @@ class ItemRecyclerAdapter (private var listener: OnItemClickListen
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
             return ItemViewHolder(
                 LayoutInflater.from(parent.context)
-                    .inflate(R.layout.departement_items_gallery_layout, parent, false)
+                    .inflate(R.layout.layout_single_list_item, parent, false)
             )
         }
 
