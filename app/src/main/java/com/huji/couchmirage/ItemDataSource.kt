92675dffce2e28bb1d7902bba526c5e577c4e939
brahanu <wassanbrahan@gmail.com>
2020-09-23 19:56:12 +0300

    still crashes

--- app/src/main/java/com/huji/couchmirage/ItemDataSource.kt
+++ app/src/main/java/com/huji/couchmirage/ItemDataSource.kt
@@ -2,35 +2,37 @@ package com.huji.couchmirage
 
 import android.util.Log
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.toObject
 
 
 class ItemDataSource {
     companion object {
         private var db = FirebaseFirestore.getInstance()
-
         fun createDataSet(department: String?): ArrayList<SingleItem> {
             val list = ArrayList<SingleItem>()
-            var check = 0
             if (department != null) {
                 db.collection(department).get()
                     .addOnSuccessListener { result ->
                         for (document in result) {
-                            check++
-                            list.add(document.toObject(SingleItem.class))
+                            list.add(document.toObject(SingleItem::class.java))
                         }
                     }
-//                   Log.d("BRUHHHH1","${document.id}=>${document.data}")
-                Log.d("BRUHHHH2", "" + check)
             }
             return list
         }
     }
 }
 
-//SingleItem( document.getString("category"),
+
+
+//                   Log.d("BRUHHHH1","${document.id}=>${document.data}")
+//                Log.d("BRUHHHH2", "" + check)
+//SingleItem(
+//document.getString("category"),
 //document.getString("color"),
-//(document.getString("details")),
+//document.getString("details"),
 //document.getString("images"),
 //document.getString("model"),
 //document.getString("price"),
 //document.getString("sizes")
+//)
